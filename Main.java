4ec34722638d27704d1aec3bd4cdb7968273b859
Elena Voinu <37947819+Lenka111@users.noreply.github.com>
2020-02-13 22:13:35 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,40 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        calcFeetAndInchesToCentimeters(6, 10);
+        calcFeetAndInchesToCentimeters(100);
+
+
+    }
+
+    //feet to inches
+    public static double calcFeetAndInchesToCentimeters(double feet, double inches){
+
+        if(feet <0 || inches <0 || inches >12) {
+            System.out.println("Invalid parameters");
+            return -1;
+        }
+
+        double cm = (feet * 12) * 2.54;
+        cm += inches * 2.54;
+        System.out.println(feet + " feet " + inches + " inches = " + cm + "cm");
+        return cm;
+
+        }// end calcFeet method
+
+    public static double calcFeetAndInchesToCentimeters(double inches) {
+        if (inches < 0) {
+            System.out.println("Invalid parameters");
+            return -1;
+        }
+        double feet = (int) inches/12;
+        double remainingInches = (int) inches % 12;
+        System.out.println(inches +"inches= " + feet + "feet" + remainingInches + "inches");
+        return  calcFeetAndInchesToCentimeters(feet, remainingInches);
+    }
+
+
+    }
+
+
